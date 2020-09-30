e1dbd695df874b7925a8a8adbf96f3842ef33370
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-09-30 16:25:26 +0900

    Implement calculator fucntion

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,10 +11,24 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var displayLabel: UILabel!
 
-    var isFinishedTypingNumber = true
+    private var isFinishedTypingNumber = true
 
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        // What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to double")
+        }
+
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            }
+        }
     }
 
     @IBAction func numButtonPressed(_ sender: UIButton) {
