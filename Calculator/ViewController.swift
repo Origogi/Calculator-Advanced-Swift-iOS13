ba2f753b9b4c9f9c1f5ba075d2b4ed25d1ae11cb
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-10-01 21:14:41 +0900

    Refactoring with Computed Properties

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,18 +13,26 @@ class ViewController: UIViewController {
 
     private var isFinishedTypingNumber = true
 
+    private var displayValue: Double {
+        get {
+            guard let currentDisplayName = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to double")
+            }
+            return currentDisplayName
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
 
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to double")
-        }
-
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayLabel.text = String(displayValue * -1)
             } else if calcMethod == "%" {
-                displayLabel.text = String(number / 100)
+                displayLabel.text = String(displayValue / 100)
             } else if calcMethod == "AC" {
                 displayLabel.text = "0"
             }
@@ -38,18 +46,13 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
             } else {
                 if numValue == "." {
-                    guard let currentDisplayName = Double(displayLabel.text!) else {
-                        fatalError("Cannot convert display label text to double")
-                    }
-                    
-                    let isInt = floor(currentDisplayName) == currentDisplayName
-                    
+                    let isInt = floor(displayValue) == displayValue
+
                     if !isInt {
                         return
                     }
                 }
-                
-                
+
                 displayLabel.text?.append(numValue)
             }
         }
